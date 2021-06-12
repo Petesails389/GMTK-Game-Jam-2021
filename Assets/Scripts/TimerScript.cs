fbcdaefb12447d693a2940bc0cc94c8529cf7baf
BuildTools <unconfigured@null.spigotmc.org>
2021-06-12 07:43:37 +0100

    players stop with the timer now

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -6,19 +6,30 @@ public class TimerScript : MonoBehaviour
 {
     [SerializeField] float time;
     [SerializeField] Transform timerForeground;
+    [SerializeField] bool timing;
+    private GameObject[] players;
+    private float currentTime;
+    private float gfxWidth;
 
     void Start()
     {
-        
+        players = GameObject.FindGameObjectsWithTag("Love");
+        currentTime = time;
+        gfxWidth = timerForeground.localScale.x;
     }
 
     void Update()
     {
-        time -= Time.deltaTime;
-        timerForeground.localScale = new Vector3(7*(time/100),0.25f,0);
+        if(timing){
+            currentTime -= Time.deltaTime;
+        }
+        timerForeground.localScale = new Vector3(gfxWidth*(currentTime/time),0.25f,0);
         timerForeground.localPosition = new Vector3((7-timerForeground.localScale.x)/(-2),timerForeground.localPosition.y,0);
-        if (time <= 0){
-            //end the game
+        if (currentTime <= 0){
+            foreach(GameObject player in players){
+                player.GetComponent<Person>().StopMoving();
+            }
+            timing = false;
         };
     }
 }
